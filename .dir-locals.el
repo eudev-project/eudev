7d4a62f8c1404ed426500b97af03d4ef8d034a71
Anthony G. Basile <blueness@gentoo.org>
2012-11-15 10:33:16 -0500

    Isolation of udev code from remaining systemd
    This commit is a first attempt to isolate the udev code from the
    remaining code base.  It intentionally does not modify any files
    but purely delete files which, on a first examination, appear to
    not be needed.  This is a sweeping commit which may easily have
    missed needed code.  Files can be retrieved by doing a checkout
    from the previous commit:
    
     git checkout 2944f347d0 -- <filename>

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-; Sets emacs variables based on mode.
-; A list of (major-mode . ((var1 . value1) (var2 . value2)))
-; Mode can be nil, which gives default values.
-
-((nil . ((indent-tabs-mode . nil)
-         (tab-width . 8)))
-)
