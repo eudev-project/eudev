8b75afdbc2c0302da3110fbc2b33bc896c39a66e
Felipe Contreras <felipe.contreras@gmail.com>
2012-11-17 21:07:58 +0100

    Fix build when gtk-doc is enabled
    Looks like gtk-doc.make is meant for automake, not make.
    
    Some previous changes need to be reverted.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@
 if type -p gtkdocize > /dev/null; then
 	gtkdocize --docdir docs
 else
+	echo 'EXTRA_DIST =' > docs/gtk-doc.make
 	echo 'AC_DEFUN([GTK_DOC_CHECK], [AM_CONDITIONAL([ENABLE_GTK_DOC], [FALSE])])' > m4/gtk-doc.m4
 fi
 
