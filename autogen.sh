b0d2945b5b387feea20c43f74914177c4f74f1ed
Felipe Contreras <felipe.contreras@gmail.com>
2012-11-17 04:26:20 +0100

    Allow build without gtk-doc
    Signed-off-by: Anthony G. Basile <blueness@gentoo.org>
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -6,8 +6,8 @@ if type -p gtkdocize > /dev/null; then
     gtkdocize --docdir docs
     gtkdocargs=--enable-gtk-doc
 fi
-  
-aclocal && \
+
+aclocal -I m4 && \
 autoheader && \
 autoconf && \
 libtoolize --copy && \
