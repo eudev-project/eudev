d10ce69cc77ab20aacdd8fa80cc47df532547921
Felipe Contreras <felipe.contreras@gmail.com>
2012-11-17 17:09:29 +0100

    autogen: cleanup
    Signed-off-by: Anthony G. Basile <blueness@gentoo.org>
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-set -e
+type -p gtkdocize > /dev/null && gtkdocize --docdir docs
 
-if type -p gtkdocize > /dev/null; then
-    gtkdocize --docdir docs
-    gtkdocargs=--enable-gtk-doc
-fi
-
-aclocal -I m4 && \
-autoheader && \
-autoconf && \
-libtoolize --copy && \
-automake --add-missing --copy
+autoreconf -f -i
