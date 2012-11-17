54cdf5fe9116577071c8e464f46353df6ba761c1
Felipe Contreras <felipe.contreras@gmail.com>
2012-11-17 21:05:42 +0100

    Truly fix build without gtk-doc
    The gtk-doc.m4 was not merged to 'master' but we don't need it. It's
    better to generate it.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-type -p gtkdocize > /dev/null && gtkdocize --docdir docs
+if type -p gtkdocize > /dev/null; then
+	gtkdocize --docdir docs
+else
+	echo 'AC_DEFUN([GTK_DOC_CHECK], [AM_CONDITIONAL([ENABLE_GTK_DOC], [FALSE])])' > m4/gtk-doc.m4
+fi
 
 autoreconf -f -i
