bcc69ef934c9d14732e16551032242fa599b44e7
Anthony G. Basile <blueness@gentoo.org>
2012-11-20 14:44:47 -0500

    Reintroduce keymaps build and test
    This commit reintroduces code to
    
     1) build src/keymap
     2) test keymap/Makefile.am that it has all the key maps listed
     3) test that all the key names in keymap/* are in <linux/input.h>
     4) do a syntax check on rules/*.rules which now includes
        95-keymap.rules and 95-keyboard-force-release.rules
    
    For #4, the regex expressions in rule-syntax-check.py had to be
    updated.  They do not allow trailing comments via # comment, and
    I did not include that.  A rule in 95-keymap.rules had to have its
    comment moved from the end to another line.
    
    Signed-off-by: Anthony G. Basile <blueness@gentoo.org>

--- src/keymap/check-keymaps.sh
+++ src/keymap/check-keymaps.sh
@@ -3,18 +3,21 @@
 # check that all key names in keymaps/* are known in <linux/input.h>
 # and that all key maps listed in the rules are valid and present in
 # Makefile.am
-SRCDIR=${1:-.}
-KEYLIST=${2:-src/udev/keymap/keys.txt}
-KEYMAPS_DIR=$SRCDIR/keymaps
-RULES=$SRCDIR/src/udev/keymap/95-keymap.rules
+
+SRCDIR=${1:-../..}
+KEYLIST=${2:-${SRCDIR}/src/keymap/keys.txt}
 
 [ -e "$KEYLIST" ] || {
         echo "need $KEYLIST please build first" >&2
         exit 1
 }
 
+KEYMAPS_DIR=${SRCDIR}/keymaps
+KEYMAPS_LIST=$(ls ${KEYMAPS_DIR}/* | grep -v Makefile)
+RULES=${SRCDIR}/rules/95-keymap.rules
+
 missing=$(join -v 2 <(awk '{print tolower(substr($1,5))}' $KEYLIST | sort -u) \
-                    <(grep -hv '^#' ${KEYMAPS_DIR}/*| awk '{print $2}' | sort -u))
+                    <(grep -hv '^#' ${KEYMAPS_LIST}| awk '{print $2}' | sort -u))
 [ -z "$missing" ] || {
         echo "ERROR: unknown key names in keymaps/*:" >&2
         echo "$missing" >&2
@@ -31,7 +34,7 @@ for m in $maps; do
                 echo "ERROR: unknown map name in $RULES: $m" >&2
                 exit 1
         }
-        grep -q "keymaps/$m\>" $SRCDIR/Makefile.am || {
+        grep -q "$m\>" ${SRCDIR}/keymaps/Makefile.am || {
                 echo "ERROR: map file $m is not added to Makefile.am" >&2
                 exit 1
         }
