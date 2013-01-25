a9ef3ca7642ae67fc6859d2b5861f8b0f0ab3a68
Ian Stakenvicius <axs@gentoo.org>
2013-01-25 13:34:21 -0500

    Make blkid optional
    Embedded systems may not want to include util-linux when they do
    not intend to use blkid functionality
    
    Modified from original author's commit to not install
    60-persistent-storage.rules if blkid is disabled
    
    systemd commit f553b3b1074151200187df916427a1468186435e
    Original author: Anders Olofsson <Anders.Olofsson@axis.com>
    
    Signed-off-by: Anthony G. Basile <blueness@gentoo.org>

--- src/udev/udev.h
+++ src/udev/udev.h
@@ -136,7 +136,9 @@ int udev_ctrl_get_set_children_max(struct udev_ctrl_msg *ctrl_msg);
 
 /* built-in commands */
 enum udev_builtin_cmd {
+#ifdef HAVE_BLKID
         UDEV_BUILTIN_BLKID,
+#endif
         UDEV_BUILTIN_BTRFS,
         UDEV_BUILTIN_FIRMWARE,
         UDEV_BUILTIN_HWDB,
@@ -158,7 +160,9 @@ struct udev_builtin {
         bool (*validate)(struct udev *udev);
         bool run_once;
 };
+#ifdef HAVE_BLKID
 extern const struct udev_builtin udev_builtin_blkid;
+#endif
 extern const struct udev_builtin udev_builtin_btrfs;
 extern const struct udev_builtin udev_builtin_firmware;
 extern const struct udev_builtin udev_builtin_hwdb;
