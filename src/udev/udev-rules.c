eudev-project/eudev

commit 601c4e8ce69338652b3c9178386cbc05de8bae8d

udev: update
This updates the daemon codebase to upstream.

Authors:
	Zbigniew Jędrzejewski-Szmek
	Lennart Poettering
	Václav Pavlín
	Kay Sievers
	Harald Hoyer
	Rob Clark
	Zeeshan Ali (Khattak)

See http://cgit.freedesktop.org/systemd/systemd/log/src/udev

Ian is signing off but has some concerns on a few of the changes
that he may fix in future commits.

Signed-off-by: Anthony G. Basile <blueness@gentoo.org>
Signed-off-by: Ian Stakenvicius <axs@gentoo.org>

src/udev/udev-rules.c
@@ -1600,21 +1600,6 @@ struct udev_rules *udev_rules_new(struct udev *udev, int resolve_names)
         if (!rules->strbuf)
                 return udev_rules_unref(rules);
 
-	/* Note - need a better way to manage these paths:
-	 * ie, should have a list of paths we always support, and
-	 * do not include the #define vars if they are duplicates
-	 *
-	 * original - UDEV_RULES_DIR should be /etc/udev/rules.d
-	 * ..but we have it set to UDEV_LIBEXEC_DIR "/rules.d" so that
-	 * ..the default rules are installed there instead of in /etc
-	 * 
-	 * original2 - UDEV_LIBEXEC_DIR used to be forced to /usr/lib/udev
-	 * ..by default, we have it as /lib/udev (or whatever rootlibdir is)
-	 * ..therefore we did not include /usr/lib/udev/rules.d.
-	 * 
-	 * Reworked code; there are duplicate paths here but at least
-	 * everyting is listed.
-	 */
         rules->dirs = strv_new(UDEV_CONF_DIR "/rules.d",
                                UDEV_RULES_DIR,
                                "/run/udev/rules.d",
@@ -1639,7 +1624,7 @@ struct udev_rules *udev_rules_new(struct udev *udev, int resolve_names)
                 return udev_rules_unref(rules);
         udev_rules_check_timestamp(rules);
 
-        r = conf_files_list_strv(&files, NULL, ".rules", (const char **)rules->dirs);
+        r = conf_files_list_strv(&files, ".rules", NULL, (const char **)rules->dirs);
         if (r < 0) {
                 log_error("failed to enumerate rules files: %s\n", strerror(-r));
                 return udev_rules_unref(rules);

src/udev/udev-builtin-firmware.c
@@ -78,8 +78,6 @@ static int builtin_firmware(struct udev_device *dev, int argc, char *argv[], boo
 {
         struct udev *udev = udev_device_get_udev(dev);
         static const char *searchpath[] = { FIRMWARE_PATH };
-        char fwencpath[UTIL_PATH_SIZE];
-        char misspath[UTIL_PATH_SIZE];
         char loadpath[UTIL_PATH_SIZE];
         char datapath[UTIL_PATH_SIZE];
         char fwpath[UTIL_PATH_SIZE];
@@ -111,23 +109,10 @@ static int builtin_firmware(struct udev_device *dev, int argc, char *argv[], boo
                         break;
         }
 
-        util_path_encode(firmware, fwencpath, sizeof(fwencpath));
-        util_strscpyl(misspath, sizeof(misspath), "/run/udev/firmware-missing/", fwencpath, NULL);
         util_strscpyl(loadpath, sizeof(loadpath), udev_device_get_syspath(dev), "/loading", NULL);
 
         if (fwfile == NULL) {
-                int err;
-
-                /* This link indicates the missing firmware file and the associated device */
                 log_debug("did not find firmware file '%s'\n", firmware);
-                do {
-                        err = mkdir_parents(misspath, 0755);
-                        if (err != 0 && err != -ENOENT)
-                                break;
-                        err = symlink(udev_device_get_devpath(dev), misspath);
-                        if (err != 0)
-                                err = -errno;
-                } while (err == -ENOENT);
                 rc = EXIT_FAILURE;
                 /*
                  * Do not cancel the request in the initrd, the real root might have
@@ -145,9 +130,6 @@ static int builtin_firmware(struct udev_device *dev, int argc, char *argv[], boo
                 rc = EXIT_FAILURE;
                 goto exit;
         }
-
-        if (unlink(misspath) == 0)
-                util_delete_path(udev, misspath);
 
         if (!set_loading(udev, loadpath, "1"))
                 goto exit;

src/udev/udev-builtin-kmod.c
@@ -37,7 +37,7 @@
 
 static struct kmod_ctx *ctx;
 
-static int load_module(struct udev *udev, char *const alias)
+static int load_module(struct udev *udev, const char *alias)
 {
 #ifdef HAVE_LIBKMOD
         struct kmod_list *list = NULL;

src/udev/udev-builtin-net_id.c
@@ -24,6 +24,8 @@
  *  - physical/geographical location of the hardware
  *  - the interface's MAC address
  *
+ * http://www.freedesktop.org/wiki/Software/systemd/PredictableNetworkInterfaceNames
+ *
  * Two character prefixes based on the type of interface:
  *   en -- ethernet
  *   wl -- wlan

src/udev/udev-ctrl.c
@@ -10,7 +10,6 @@
  */
 
 #include <errno.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -191,24 +190,8 @@ struct udev_ctrl_connection *udev_ctrl_get_connection(struct udev_ctrl *uctrl)
 
 #if HAVE_DECL_ACCEPT4
         conn->sock = accept4(uctrl->sock, NULL, NULL, SOCK_CLOEXEC|SOCK_NONBLOCK);
-
-        /* Fallback path when accept4() is unavailable */
-        if ( conn->sock < 0 && (errno == ENOSYS || errno == ENOTSUP) )
-        {
-                conn->sock = accept(uctrl->sock, NULL, NULL);
-
-                if (conn->sock >= 0) {
-                        fcntl(conn->sock, F_SETFL, O_NONBLOCK);
-                        fcntl(conn->sock, F_SETFD, FD_CLOEXEC);
-                }
-        }
 #else
         conn->sock = accept(uctrl->sock, NULL, NULL);
-
-        if (conn->sock >= 0) {
-                fcntl(conn->sock, F_SETFL, O_NONBLOCK);
-                fcntl(conn->sock, F_SETFD, FD_CLOEXEC);
-        }
 #endif
 
         if (conn->sock < 0) {

src/udev/udev-event.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2010 Kay Sievers <kay@vrfy.org>
+ * Copyright (C) 2003-2013 Kay Sievers <kay@vrfy.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -845,6 +845,8 @@ int udev_event_execute_rules(struct udev_event *event, struct udev_rules *rules,
                 }
 
                 if (major(udev_device_get_devnum(dev)) > 0) {
+                        bool apply;
+
                         /* remove/update possible left-over symlinks from old database entry */
                         if (event->dev_db != NULL)
                                 udev_node_update_old_links(dev, event->dev_db);
@@ -868,7 +870,8 @@ int udev_event_execute_rules(struct udev_event *event, struct udev_rules *rules,
                                 }
                         }
 
-                        udev_node_add(dev, event->mode, event->uid, event->gid);
+                        apply = streq(udev_device_get_action(dev), "add") || event->owner_set || event->group_set || event->mode_set;
+                        udev_node_add(dev, apply, event->mode, event->uid, event->gid);
                 }
 
                 /* preserve old, or get new initialization timestamp */

src/udev/udev-node.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2010 Kay Sievers <kay@vrfy.org>
+ * Copyright (C) 2003-2013 Kay Sievers <kay@vrfy.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,15 +31,13 @@
 
 #include "udev.h"
 
-#define TMP_FILE_EXT                ".udev-tmp"
-
-static int node_symlink(struct udev *udev, const char *node, const char *slink)
+static int node_symlink(struct udev_device *dev, const char *node, const char *slink)
 {
         struct stat stats;
         char target[UTIL_PATH_SIZE];
         char *s;
         size_t l;
-        char slink_tmp[UTIL_PATH_SIZE + sizeof(TMP_FILE_EXT)];
+        char slink_tmp[UTIL_PATH_SIZE + 32];
         int i = 0;
         int tail = 0;
         int err = 0;
@@ -101,7 +99,7 @@ static int node_symlink(struct udev *udev, const char *node, const char *slink)
         }
 
         log_debug("atomically replace '%s'\n", slink);
-        util_strscpyl(slink_tmp, sizeof(slink_tmp), slink, TMP_FILE_EXT, NULL);
+        util_strscpyl(slink_tmp, sizeof(slink_tmp), slink, ".tmp-", udev_device_get_id_filename(dev), NULL);
         unlink(slink_tmp);
         do {
                 err = mkdir_parents_label(slink_tmp, 0755);
@@ -204,7 +202,7 @@ static void link_update(struct udev_device *dev, const char *slink, bool add)
                         util_delete_path(udev, slink);
         } else {
                 log_debug("creating link '%s' to '%s'\n", slink, target);
-                node_symlink(udev, target, slink);
+                node_symlink(dev, target, slink);
         }
 
         if (add) {
@@ -254,7 +252,7 @@ void udev_node_update_old_links(struct udev_device *dev, struct udev_device *dev
         }
 }
 
-static int node_fixup(struct udev_device *dev, mode_t mode, uid_t uid, gid_t gid)
+static int node_permissions_apply(struct udev_device *dev, bool apply, mode_t mode, uid_t uid, gid_t gid)
 {
         const char *devnode = udev_device_get_devnode(dev);
         dev_t devnum = udev_device_get_devnum(dev);
@@ -279,13 +277,7 @@ static int node_fixup(struct udev_device *dev, mode_t mode, uid_t uid, gid_t gid
                 goto out;
         }
 
-        /*
-         * Set permissions and selinux file context only on add events. We always
-         * set it on bootup (coldplug) with "trigger --action=add" for all devices
-         * and for any newly added devices (hotplug). We don't want to change it
-         * later, in case something else has applied custom settings in the meantime.
-         */
-        if (strcmp(udev_device_get_action(dev), "add") == 0) {
+        if (apply) {
                 if ((stats.st_mode & 0777) != (mode & 0777) || stats.st_uid != uid || stats.st_gid != gid) {
                         log_debug("set permissions %s, %#o, uid=%u, gid=%u\n", devnode, mode, uid, gid);
                         chmod(devnode, mode);
@@ -294,7 +286,6 @@ static int node_fixup(struct udev_device *dev, mode_t mode, uid_t uid, gid_t gid
                 } else {
                         log_debug("preserve permissions %s, %#o, uid=%u, gid=%u\n", devnode, mode, uid, gid);
                 }
-
                 label_fix(devnode, true, false);
         }
 
@@ -304,23 +295,22 @@ out:
         return err;
 }
 
-void udev_node_add(struct udev_device *dev, mode_t mode, uid_t uid, gid_t gid)
+void udev_node_add(struct udev_device *dev, bool apply, mode_t mode, uid_t uid, gid_t gid)
 {
-        struct udev *udev = udev_device_get_udev(dev);
         char filename[UTIL_PATH_SIZE];
         struct udev_list_entry *list_entry;
 
         log_debug("handling device node '%s', devnum=%s, mode=%#o, uid=%d, gid=%d\n",
                   udev_device_get_devnode(dev), udev_device_get_id_filename(dev), mode, uid, gid);
 
-        if (node_fixup(dev, mode, uid, gid) < 0)
+        if (node_permissions_apply(dev, apply, mode, uid, gid) < 0)
                 return;
 
         /* always add /dev/{block,char}/$major:$minor */
         snprintf(filename, sizeof(filename), "/dev/%s/%u:%u",
                  strcmp(udev_device_get_subsystem(dev), "block") == 0 ? "block" : "char",
                  major(udev_device_get_devnum(dev)), minor(udev_device_get_devnum(dev)));
-        node_symlink(udev, udev_device_get_devnode(dev), filename);
+        node_symlink(dev, udev_device_get_devnode(dev), filename);
 
         /* create/update symlinks, add symlinks to name index */
         udev_list_entry_foreach(list_entry, udev_device_get_devlinks_list_entry(dev))

src/udev/udev.h
@@ -95,7 +95,7 @@ void udev_watch_end(struct udev *udev, struct udev_device *dev);
 struct udev_device *udev_watch_lookup(struct udev *udev, int wd);
 
 /* udev-node.c */
-void udev_node_add(struct udev_device *dev, mode_t mode, uid_t uid, gid_t gid);
+void udev_node_add(struct udev_device *dev, bool apply, mode_t mode, uid_t uid, gid_t gid);
 void udev_node_remove(struct udev_device *dev);
 void udev_node_update_old_links(struct udev_device *dev, struct udev_device *dev_old);
 

commit b8c77fcd0774e0c1ecf0451c4faaa4de08359ee6

src/udev/udev-rules.c: fix bug introduced in our 601c4e8
This error was introduced in our commit

    601c4e8ce69338652b3c9178386cbc05de8bae8d

when importing from upstream.  The revert line works only
in the context of all of upstream's commit

    7850b3b83791ba0e2377ba40383c5abc258b839d

Signed-off-by: Anthony G. Basile <blueness@gentoo.org>
Signed-off-by: Ian Stakenvicius <axs@gentoo.org>

src/udev/udev-rules.c
@@ -1624,7 +1624,7 @@ struct udev_rules *udev_rules_new(struct udev *udev, int resolve_names)
                 return udev_rules_unref(rules);
         udev_rules_check_timestamp(rules);
 
-        r = conf_files_list_strv(&files, ".rules", NULL, (const char **)rules->dirs);
+        r = conf_files_list_strv(&files, NULL, ".rules", (const char **)rules->dirs);
         if (r < 0) {
                 log_error("failed to enumerate rules files: %s\n", strerror(-r));
                 return udev_rules_unref(rules);