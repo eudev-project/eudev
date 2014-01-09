079a9c8229692c8c68899a9ab06693f830c1a091
Thomas Hindoe Paaboel Andersen <phomes@gmail.com>
2014-01-09 16:42:19 -0500

    Add more _printf_'s for format-nonliterals
    Clang is a bit more strict wrt format-nonliterals:
    http://clang.llvm.org/docs/LanguageExtensions.html#format-string-checking
    
    Adding these extra printf attributes also makes gcc able to find more
    problems. E.g. this patch uncovers a format issue in udev-builtin-path_id.c
    
    Some parts looked intetional about breaking the format-nonliteral check.
    I added some supression for warnings there.
    
    Signed-off-by: Anthony G. Basile <blueness@gentoo.org>

--- src/cdrom_id/cdrom_id.c
+++ src/cdrom_id/cdrom_id.c
@@ -43,6 +43,7 @@
 
 static bool debug;
 
+_printf_(6,0)
 static void log_fn(struct udev *udev, int priority,
                    const char *file, int line, const char *fn,
                    const char *format, va_list args)
