c597413eb3b84b24bc532331a4ab788c656ec0e2
Lennart Poettering <lennart@poettering.net>
2014-08-11 20:13:38 +0200

    udev: never bypass our own logging framework and call vsyslog() directly from udev tools
    Signed-off-by: Anthony G. Basile <blueness@gentoo.org>

--- src/ata_id/ata_id.c
+++ src/ata_id/ata_id.c
@@ -414,7 +414,7 @@ static void log_fn(struct udev *udev, int priority,
                    const char *file, int line, const char *fn,
                    const char *format, va_list args)
 {
-        vsyslog(priority, format, args);
+        log_metav(priority, file, line, fn, format, args);
 }
 
 int main(int argc, char *argv[])
@@ -439,11 +439,12 @@ int main(int argc, char *argv[])
                 {}
         };
 
+        log_open();
+
         udev = udev_new();
         if (udev == NULL)
                 goto exit;
 
-        log_open();
         udev_set_log_fn(udev, log_fn);
 
         while (1) {
