3b717594600fa717cdf9bcfd0c7c1b703b245482
Kay Sievers <kay@vrfy.org>
2014-09-01 09:07:49 -0400

    udev: remove userspace firmware loading support
    Signed-off-by: Anthony G. Basile <blueness@gentoo.org>

--- src/udev/udev.h
+++ src/udev/udev.h
@@ -141,9 +141,6 @@ enum udev_builtin_cmd {
         UDEV_BUILTIN_BLKID,
 #endif
         UDEV_BUILTIN_BTRFS,
-#ifdef HAVE_FIRMWARE
-        UDEV_BUILTIN_FIRMWARE,
-#endif
         UDEV_BUILTIN_HWDB,
         UDEV_BUILTIN_INPUT_ID,
 #ifdef ENABLE_KEYMAP
@@ -170,9 +167,6 @@ struct udev_builtin {
 extern const struct udev_builtin udev_builtin_blkid;
 #endif
 extern const struct udev_builtin udev_builtin_btrfs;
-#ifdef HAVE_FIRMWARE
-extern const struct udev_builtin udev_builtin_firmware;
-#endif
 extern const struct udev_builtin udev_builtin_hwdb;
 extern const struct udev_builtin udev_builtin_input_id;
 #ifdef ENABLE_KEYMAP
