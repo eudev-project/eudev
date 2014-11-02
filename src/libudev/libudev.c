daaa2f764b7b30f91820c36c1891321c72db9a48
Colin Walters <walters@verbum.org>
2014-11-02 14:38:03 -0500

    libudev: Use correct free function
    FILE * wants cleanup_fclose().
    
    Spotted by udev hwdb segfaulting in gnome-continuous' buildroot
    construction.
    
    Signed-off-by: Anthony G. Basile <blueness@gentoo.org>

--- src/libudev/libudev.c
+++ src/libudev/libudev.c
@@ -118,7 +118,7 @@ _public_ void udev_set_userdata(struct udev *udev, void *userdata)
 _public_ struct udev *udev_new(void)
 {
         struct udev *udev;
-        _cleanup_free_ FILE *f;
+        _cleanup_fclose_ FILE *f = NULL;
 
         udev = new0(struct udev, 1);
         if (udev == NULL)
