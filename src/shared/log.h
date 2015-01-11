236bd1a03ee8619fd22503af2416828e6f228fec
David Herrmann <dh.herrmann@gmail.com>
2015-01-11 03:13:46 +0100

    log: fix log_full_errno() with custom facilities
    Make sure to extract the log-priority when comparing against
    max-log-level, otherwise, we will always drop those messages.
    
    This fixes bus-proxyd to properly send warnings on policy blocks.
    
    Signed-off-by: Anthony G. Basile <blueness@gentoo.org>

--- src/shared/log.h
+++ src/shared/log.h
@@ -92,7 +92,7 @@ noreturn void log_assert_failed_unreachable(
 #define log_full_errno(level, error, ...)                                         \
         ({                                                                        \
                 int _l = (level), _e = (error);                                   \
-                (log_get_max_level() >= _l)                                       \
+                (log_get_max_level() >= LOG_PRI(_l))                              \
                 ? log_internal(_l, _e, __FILE__, __LINE__, __func__, __VA_ARGS__) \
                 : -abs(_e); \
         })
