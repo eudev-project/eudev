54d0df107e54fe74fd88bb271c4c51aff33e1858
Boian Bonev <bbonev@ipacct.com>
2022-06-09 20:08:33 +0300

    Use snprintf (for posterity)
    Also fix code style of sizeof()
    
    Suggested-by: Arsen Arsenović <arsen@aarsen.me>

--- src/udev/udev-ctrl.c
+++ src/udev/udev-ctrl.c
@@ -262,7 +262,7 @@ static int ctrl_send(struct udev_ctrl *uctrl, enum udev_ctrl_msg_type type, int 
         int err = 0;
 
         memzero(&ctrl_msg_wire, sizeof(struct udev_ctrl_msg_wire));
-        sprintf(ctrl_msg_wire.version, "%.*s", (int)(sizeof ctrl_msg_wire.version - 1), "udev-" VERSION);
+        snprintf(ctrl_msg_wire.version, sizeof(ctrl_msg_wire.version), "%.*s", (int)(sizeof(ctrl_msg_wire.version) - 1), "udev-" VERSION);
         ctrl_msg_wire.magic = UDEV_CTRL_MAGIC;
         ctrl_msg_wire.type = type;
 
