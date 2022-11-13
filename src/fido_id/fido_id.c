0d94554d9e1104092cf0708558cc44f997c5177a
Ralph Ronnquist <ralph.ronnquist@gmail.com>
2022-11-13 03:42:29 +0200

    Rewrite using current API

--- src/fido_id/fido_id.c
+++ src/fido_id/fido_id.c
@@ -19,84 +19,18 @@
 #include <unistd.h>
 
 
-//#include "libudev.h"
-//#include "libudev-private.h"
-//#include "fido_id_desc.h"
-//#include "udev-util.h"
-
-
-#include "libudev.h"
-#include "libudev-private.h"
-//#include "device-private.h"
-//#include "device-util.h"
-//#include "fd-util.h"
 #include "fido_id_desc.h"
+#include "libudev-private.h"
+#include "libudev.h"
 #include "log.h"
 #include "macro.h"
-//#include "main-func.h"
 #include "path-util.h"
-//#include "string-util.h"
-#include "udev.h"
 #include "udev-util.h"
-#include "libudev.h"
-#include "libudev-private.h"
+#include "udev.h"
 
-int device_new_aux(struct udev_device **ret) {
-        struct udev_device *device;
-
-        assert(ret);
-
-        device = new(struct udev_device, 1);
-        if (!device)
-                return -ENOMEM;
-
-        *device = (struct udev_device) {
-                .n_ref = 1,
-                .watch_handle = -1,
-                .devmode = MODE_INVALID,
-                .devuid = UID_INVALID,
-                .devgid = GID_INVALID,
-                .action = _SD_DEVICE_ACTION_INVALID,
-        };
-
-        *ret = device;
-        return 0;
-}
-
-int device_new_from_strv(struct udev_device **ret, char **strv) {
-        _cleanup_(udev_device_unrefp) struct udev_device *device = NULL;
-        const char *major = NULL, *minor = NULL;
-        int r;
-
-        assert(ret);
-        assert(strv);
-
-        r = device_new_aux(&device);
-        if (r < 0)
-                return r;
-
-        STRV_FOREACH(key, strv) {
-                r = device_append(device, *key, &major, &minor);
-                if (r < 0)
-                        return r;
-        }
-
-        if (major) {
-                r = device_set_devnum(device, major, minor);
-                if (r < 0)
-                        return log_device_debug_errno(device, r, "sd-device: Failed to set devnum %s:%s: %m", major, minor);
-        }
-
-        r = device_verify(device);
-        if (r < 0)
-                return r;
-
-        *ret = TAKE_PTR(device);
-
-        return 0;
-}
 
 int main(int argc, char **argv) {
+        struct udev *config = 0;
         _cleanup_(udev_device_unrefp) struct udev_device *device = NULL;
         _cleanup_free_ char *desc_path = NULL;
         _cleanup_close_ int fd = -1;
@@ -117,43 +51,43 @@ int main(int argc, char **argv) {
                 return log_error_errno(/*SYNTHETIC_ERRNO*/(EINVAL), "Usage: %s [SYSFS_PATH]", program_invocation_short_name);
 
         if (argc == 1) {
-                r = device_new_from_strv(&device, environ);
-                if (r < 0)
-                        return log_error_errno(r, "Failed to get current device from environment: %m");
+                device = udev_device_new_from_environment( config );
+                if ( device == NULL )
+                        return log_error_errno(errno, "Failed to get current device from environment: %m");
         } else {
-                r = udev_device_new_from_syspath(&device, argv[1]);
-                if (r < 0)
+                device = udev_device_new_from_syspath( config, argv[1] );
+                if ( device == NULL )
                         return log_error_errno(r, "Failed to get device from syspath: %m");
         }
 
-        r = udev_device_get_parent(device, &hid_device);
-        if (r < 0)
-                return log_device_error_errno(device, r, "Failed to get parent HID device: %m");
+        hid_device = udev_device_get_parent( device );
+        if ( hid_device == NULL )
+                return log_error_errno(errno, "Failed to get parent HID device: %m");
 
-        r = udev_device_get_syspath(hid_device, &sys_path);
-        if (r < 0)
-                return log_device_error_errno(hid_device, r, "Failed to get syspath for HID device: %m");
+        sys_path = udev_device_get_syspath( hid_device );
+        if ( sys_path == NULL )
+                return log_error_errno(errno, "Failed to get syspath for HID device: %m");
 
-        desc_path = path_join(sys_path, "report_descriptor");
-        if (!desc_path)
+        desc_path = path_join( sys_path, "report_descriptor" );
+        if ( desc_path == NULL )
                 return log_oom();
 
         fd = open(desc_path, O_RDONLY | O_NOFOLLOW | O_CLOEXEC);
         if (fd < 0)
-                return log_device_error_errno(hid_device, errno,
+                return log_error_errno(errno,
                                               "Failed to open report descriptor at '%s': %m", desc_path);
 
         desc_len = read(fd, desc, sizeof(desc));
         if (desc_len < 0)
-                return log_device_error_errno(hid_device, errno,
+                return log_error_errno(errno,
                                               "Failed to read report descriptor at '%s': %m", desc_path);
         if (desc_len == 0)
-                return log_device_debug_errno(hid_device, /*SYNTHETIC_ERRNO*/(EINVAL),
+                return log_debug_errno(/*SYNTHETIC_ERRNO*/(EINVAL),
                                               "Empty report descriptor at '%s'.", desc_path);
 
         r = is_fido_security_token_desc(desc, desc_len);
         if (r < 0)
-                return log_device_debug_errno(hid_device, r,
+                return log_debug_errno(errno,
                                               "Failed to parse report descriptor at '%s'.", desc_path);
         if (r > 0) {
                 printf("ID_FIDO_TOKEN=1\n");
